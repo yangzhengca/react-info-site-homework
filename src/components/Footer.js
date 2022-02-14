08654b8296c98ce218117d7c54f0b22ba9718b1c
yangzhengca <yangzhengca>
2022-02-14 15:27:46 -0600

    finished V1.0

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <a className='social-icon twitter'><i className='fa fa-twitter-square'></i></a>
+      <a className='social-icon facebook'><i className='fa fa-facebook-square'></i></a>
+      <a className='social-icon instagram'><i className='fa fa-github'></i></a>
+      <a><i className='fa fa-github-square' ></i></a>
+    </div>
   )
 }
 
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Interests = () => {
   return (
-    <div>Interests</div>
+    <div className='interests'>
+      <h2 className='interests-title'>Interests</h2>
+      <p className='interests-content'>
+      Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
   )
 }
 
